af72ecef4d2075dfd77b9eb5d9824ad0cf90820e
montanogilberto <montanogilberto@gmail.com>
2024-10-25 18:59:13 -0700

    adding style menu 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let isSliding = false;
-let firstSlideDuration = 60000; // 20 seconds
-let otherSlidesDuration = 45000; // 15 seconds
+let firstSlideDuration = 60000; // 60 seconds
+let otherSlidesDuration = 45000; // 45 seconds
 let currentSlideIndex = 0;
 
 function setSlideInterval() {
@@ -37,3 +37,15 @@ $('#menuCarousel2').on('slide.bs.carousel', function (event) {
 
 // Set the initial interval for the first slide
 setSlideInterval();
+
+// Generate QR code for the menu URL
+const qrCodeUrl = "https://icy-hill-0ec54941e.5.azurestaticapps.net/"; // Your menu URL
+
+let qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: qrCodeUrl,    // URL to be embedded in the QR code
+    width: 200,         // Width of the QR code
+    height: 200,        // Height of the QR code
+    colorDark: "#000000", // Dark color for the QR code
+    colorLight: "#ffffff", // Light color (background)
+    correctLevel: QRCode.CorrectLevel.H // Error correction level
+});
